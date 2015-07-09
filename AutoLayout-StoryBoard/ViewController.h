f695c1741dfde0bc95f158a574fdf405bfe954c9
rcacho <rcacho@hotmail.ca>
2015-07-08 19:07:01 -0700

    autolayout done through storyboard

---
+++ AutoLayout-StoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout-StoryBoard
+//
+//  Created by ricardo antonio cacho on 2015-07-08.
+//  Copyright (c) 2015 ricardo antonio cacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
